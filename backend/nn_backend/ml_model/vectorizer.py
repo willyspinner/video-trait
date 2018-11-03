88bb50994e1621520db3547b0d2c459f43dffa37
willyspinner <wilsonjusuf1998@gmail.com>
2018-11-03 11:30:26 -0700

    training should work now. Tried with test data

--- backend/nn_backend/ml_model/vectorizer.py
+++ backend/nn_backend/ml_model/vectorizer.py
@@ -11,33 +11,33 @@ model = None
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-class fakeModel:
-    def wv(self, word):
-        return np.random.random(200)
-
-if os.environ['ENVIRON'] == "production":
-    glove_path = path.join(path.dirname(path.abspath(__file__)), '..', 'dataset','glove.6B', 'glove.6B.200d.txt')
-    glove_w2v_path = path.join(path.dirname(path.abspath(__file__)),'..','dataset','glove.6B','glove.6B.200d.w2v.txt')
-    if not path.isfile(glove_w2v_path):
-        eprint("w2v for glove doesn't exist.. making it w2v format...")
-        glove2word2vec(glove_path, glove_w2v_path)
-        eprint("w2v format conversion is done.")
-    else:
-        eprint("w2v for glove exists.. skipping format conversion...")
-    eprint("w2v loading glove w2v model... This will take a long time...")
-    model = KeyedVectors.load_word2vec_format(glove_w2v_path, binary=False)
-    eprint("w2v loading done.")
-    # calculate: (king - man) + woman = ?
-    #  result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
-    #  eprint(result)
+glove_path = path.join(path.dirname(path.abspath(__file__)), '..', 'dataset','glove.6B', 'glove.6B.200d.txt')
+glove_w2v_path = path.join(path.dirname(path.abspath(__file__)),'..','dataset','glove.6B','glove.6B.200d.w2v.txt')
+if not path.isfile(glove_w2v_path):
+    eprint("w2v for glove doesn't exist.. making it w2v format...")
+    glove2word2vec(glove_path, glove_w2v_path)
+    eprint("w2v format conversion is done.")
 else:
-    eprint("using fake w2v to speed up development time...")
-    model = fakeModel()
+    eprint("w2v for glove exists.. skipping format conversion...")
+eprint("w2v loading glove w2v model... This will take a long time...")
+if os.environ['ENVIRON'] == "production":
+    model = KeyedVectors.load_word2vec_format(glove_w2v_path, binary=False)
+else:
+    model = KeyedVectors.load_word2vec_format(glove_w2v_path, binary=False, limit=5000)
+eprint("w2v loading done.")
+
+def get_vector(word, dims):
+    vec = None
+    try:
+        vec = model[word]
+    except KeyError:
+        vec = np.zeros(dims)
+    return vec
 
 
-def vectorize_words(sentence_str, dims, max_timesteps):
+def vectorize_words(sentence_str, max_timesteps, dims):
     # TODO: dims and max_timesteps.
-    words = sentence_str.split(',"')[1].split(" ")[1:-1] # tokenization by space.
+    words = sentence_str.split(',"')[1].split(" ")[1:-1][:max_timesteps] # tokenization by space.
 
     # filter out punctuation
     table = str.maketrans('', '', string.punctuation)
@@ -49,7 +49,8 @@ def vectorize_words(sentence_str, dims, max_timesteps):
     # TODO: clean it up more here?
     # https://machinelearningmastery.com/clean-text-machine-learning-python/
     # vectorize it here.
-    x = np.array([model.wv(word) for word in words][:max_timesteps])
+
+    x = np.row_stack(tuple([get_vector(word, dims) for word in words]))
     # x is timesteps x dims. Yes: wv is a dim dimensional vector.
     if x.shape[1] != dims:
         eprint("ERROR! x columns doesn't equal dims.")
--- backend/nn_backend/train.py
+++ backend/nn_backend/train.py
@@ -4,24 +4,32 @@ from os import path
 import sys
 import h5py
 from keras.models import model_from_json
-from ml_model.vectorizer import vectorize_words
+import numpy as np
+from keras import optimizers
 from ml_model.PersonalityVector import to_numpy_class_vector
 from ml_model.LSTM import createLSTMModel
-
-# ********** config ***********
-max_timesteps = 30 # words
-dims = 200
-mbti_classes = 16
-# **************** paths *************************
-# dataset path
+from keras.callbacks import ReduceLROnPlateau
+# ********* dataset path ************
 raw_dataset_path = path.join(path.dirname(path.abspath(__file__)), 'dataset', 'mbti_1.csv')
 vectorized_dataset_X_path = path.join(path.dirname(path.abspath(__file__)), 'dataset', 'mbti_1_vectorized_X.h5')
 vectorized_dataset_Y_path = path.join(path.dirname(path.abspath(__file__)), 'dataset', 'mbti_1_vectorized_Y.h5')
+# ********* config ************
+max_timesteps = 30 # words
+dims = 200
+mbti_classes = 16
+num_datapoints = 100  #   sum(1 for line in open(raw_dataset_path)) - 1  # 1 for the initial type,post
 
-#
-# weights n arch
-weights_path = path.join(path.dirname(path.abspath(__file__)),'ml_model','weights')
-architecture_path = path.join(path.dirname(path.abspath(__file__)),'ml_model','architectures')
+# ********* tuning ************
+traintest_split = 0.8 # train test split
+learning_rate = 0.1
+epochs=10
+steps_per_epoch=10
+
+# ********* weights and arch ************
+weights_path = path.join(path.dirname(path.abspath(__file__)), 'ml_model', 'weights')
+architecture_path = path.join(path.dirname(path.abspath(__file__)), 'ml_model', 'architectures')
+model_weights_path = path.join(weights_path,"lstm-weights.h5")
+model_architecture_path= path.join(architecture_path,"lstm.json")
 
 model = None
 
@@ -30,8 +38,8 @@ def eprint(*args, **kwargs):
 
 # *****************  generate vectorized ndarray of dataset if not exists. *******************
 if not (path.isfile(vectorized_dataset_X_path) and path.isfile(vectorized_dataset_Y_path)):
+    from ml_model.vectorizer import vectorize_words
     # make the data here. vectorize it
-    num_datapoints = sum(1 for line in open(raw_dataset_path)) - 1  # 1 for the initial type,post
     X_dset = None
     Y_dset = None
     with h5py.File(vectorized_dataset_X_path, "a") as f_X:
@@ -42,14 +50,14 @@ if not (path.isfile(vectorized_dataset_X_path) and path.isfile(vectorized_datase
             raw_dataset = open(raw_dataset_path, "r")
             count = 0
             for line in raw_dataset:
-                # skip initial
-                # data is     TYPE,"word word word .. etc..."
+                if count >= num_datapoints:
+                    break
                 personality = line[:4]
                 if personality == "type":
                     continue
                 y = to_numpy_class_vector(personality)
                 # now find x. should have dims: n of words (rows) x dim (columns).
-                x = vectorize_words(line, dims, max_timesteps)
+                x = vectorize_words(line, max_timesteps, dims)
                 X_dset[count, :, :] = x
                 Y_dset[count] = y
                 eprint("done processing line ", count, "out of ", num_datapoints)
@@ -66,21 +74,60 @@ else:
 
 
 # make the architecture of the model.
-archPath = createLSTMModel()
+archPath = createLSTMModel(max_timesteps, dims, mbti_classes, model_architecture_path, 0.001)
 
 
 # *****************  train it using dataset  *******************
+indices = [i for i in range(0, num_datapoints)]
+np.random.shuffle(indices)
+def generator(is_train=True, split=0.8):
+    X = h5py.File(vectorized_dataset_X_path, "r")
+    Y = h5py.File(vectorized_dataset_Y_path, "r")
+    low_bound = 0
+    high_bound = num_datapoints
+    if is_train:
+        high_bound = int(split * num_datapoints)
+    else:
+        low_bound = int(split * num_datapoints)
+    while True:
+        for i in indices[low_bound:high_bound]:
+            yield X.get('autochunk')[i, :, :].reshape(1,max_timesteps,dims), Y.get('autochunk')[i].reshape(1, mbti_classes)
+
 
 archFile = open(archPath,'r')
-def vec_generator (csv_path):
-    print("TODO")
 model = model_from_json(archFile.read())
+adamax = optimizers.Adamax(lr=learning_rate)
+model.compile(loss='binary_crossentropy', #categorical_crossentropy
+                   optimizer=adamax,#'adadelta',
+                   metrics=['accuracy'])
+callbacks = [ReduceLROnPlateau(monitor='accuracy', factor=0.2,
+    patience=3, min_lr=0.001)]
+model.fit_generator(
+    generator(is_train=True, split=traintest_split),
+    steps_per_epoch=steps_per_epoch,
+    epochs=epochs,
+    verbose=1,   # progress bar
+    use_multiprocessing=True,
+    callbacks=callbacks
+    )
 
 
 
-# evaluate it
+# *****************  evaluate it *******************
 
+eprint("evaluating....")
+val  =model.evaluate_generator(
+   generator(is_train=False ),
+    verbose=1,
+    steps=10
+)
+eprint(model.metrics_names)
+eprint(val)
 
 # save it - output to stdout the paths of the weights and architectures.
+model.save_weights(model_weights_path)
 
+eprint("saved at:")
+print(model_weights_path)
+print(model_architecture_path)
 
