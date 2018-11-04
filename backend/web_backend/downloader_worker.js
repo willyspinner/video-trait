66c3849616af09995089f583b723cf8c7f1357c7
willyspinner <wilsonjusuf1998@gmail.com>
2018-11-04 04:34:08 -0800

    more console loggy log logs

--- backend/web_backend/downloader_worker.js
+++ backend/web_backend/downloader_worker.js
@@ -4,6 +4,7 @@ const child_process =require('child_process');
 //var worker = require('child_process');
 const work = (vid_id)=>{
     return new Promise((resolve,reject)=>{
+        console.log(`worker processing video ${vid_id}`)
         let work_process = child_process.spawn("./ytdl.sh",[vid_id]);
         work_process.on('error', (err)=>{
             console.log(err);
@@ -11,6 +12,7 @@ const work = (vid_id)=>{
             return;
         })
         work_process.on('close',(code)=>{
+        console.log(`worker processing video ${vid_id} closed. now finding the paths for gcp...`)
             if (code !== 0){
                 //handle here.
                 reject(code);
@@ -18,14 +20,19 @@ const work = (vid_id)=>{
             }
             sortedPath =[];
             const createSorted = child_process.spawn(`find $(pwd)/frames -max-depth 1 -type f | sort`);
+            createSorted.stderr.on('data', (data)=>{
+                console.error("FIND ERR TING", data.toString);
+            })
             createSorted.stdout.on('data', (filepath)=>{
                 sortedPath.append(filepath.toString);
             })
             createSorted.stdout.on('close', () =>  {   
+            console.log(`worker processing video ${vid_id} closed path finding. Now doing label detection in GCP...`)
                 const client = new vision.ImageAnnotatorClient();
                 Promise.all(
                     sortedPath.map((path=>client.labelDetection(path)))
                 ).then((aggregatedResults)=>{
+                    console.log(`worker processing video ${vid_id} Done. Finally exiting.....`)
                     let full_video_sentence = aggregatedResults.map(results=>{
                         //do something with this individual result.
                             const labels = results[0].labelAnnotations.join(" ");
